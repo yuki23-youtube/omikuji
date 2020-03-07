1a6193638bca5fae86507686757d9b62171ae364
yuki23-youtube <61912946+yuki23-youtube@users.noreply.github.com>
2020-03-08 00:48:24 +0900

    first omikuji prototype

---
+++ Assets/Scripts/OmikujiBoxScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OmikujiBoxScript : MonoBehaviour
+{
+    private string goodLuck = "大吉";
+    private string badLuck = "凶";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string NotifyOmikujiResult()
+    {
+        int selectedLuck = Random.Range(0, 2);
+        if (selectedLuck == 0)
+            return badLuck;
+        return goodLuck;
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            DrawOmikuji();
+        }
+    }
+
+    void DrawOmikuji()
+    {
+        string omikujiResult = GameObject.Find("OmikujiBox").GetComponent<OmikujiBoxScript>().NotifyOmikujiResult();
+        Debug.Log(omikujiResult);
+    }
+}
